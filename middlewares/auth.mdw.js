52d9516f95d2d59ba2847c795b34bca45e8dcfab
HuyDeveloper <vinhhuy16072002@gmail.com>
2023-05-16 23:10:46 +0700

    session and fix login register

---
+++ middlewares/auth.mdw.js
@@ -0,0 +1,7 @@
+export default function (req, res, next) {
+    if (req.session.auth === false) {
+        req.session.retUrl = req.originalUrl;
+        return res.redirect('/account/login');
+    }
+    next();
+}
