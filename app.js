a87531a189132c339d0ae1c29a5cf19bbb3f60e7
Dương Song <duongsong9a4@gmail.com>
2023-05-16 22:09:12 +0700

    Fix the merge of app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import hbs_sections from "express-handlebars-sections";
 import { engine } from "express-handlebars";
-
+import numeral from 'numeral'
 import accountRoute from "./routes/account.route.js";
+import adminRoute from "./routes/admin.route.js";
+import cartRoute from "./routes/cart.route.js";
+import productsUserRoute from "./routes/products-user.route.js";
 const app = express();
 app.use(
     express.urlencoded({
@@ -13,10 +16,29 @@ app.use(
 app.engine(
     "hbs",
     engine({
+        // defaultLayout: 'main.hbs'
         extname: "hbs",
         defaultLayout: "main",
         helpers: {
             section: hbs_sections(),
+            format_number(val) {
+                return numeral(val).format("0,0");
+            },
+            eq(arg1, arg2) {
+                return +arg1 === +arg2;
+            },
+            minus(a, b) {
+                return a - b;
+            },
+            add(a, b) {
+                return a + b;
+            },
+            eqString(arg1, arg2) {
+                if (arg1.localeCompare(arg2) === 0) {
+                    return true;
+                }
+                return false;
+            },
         },
     })
 );
@@ -28,6 +50,9 @@ app.get("/", function (req, res){
     res.render("home")
 })
 app.use("/account", accountRoute)
+app.use("/admin", adminRoute)
+app.use("/cart", cartRoute)
+app.use("/product", productsUserRoute)
 const PORT = 3000;
 app.listen(PORT, function () {
     console.log(`E-commerce application listening at http://localhost:${PORT}`);
